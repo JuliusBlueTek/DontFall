c13d001ee2d406e47733d408cd08ccfc9faa0253
Julius Jockusch <bluestek@gmail.com>
2018-02-04 15:47:16 -0800

    Setup intro scene and built started building out vr controllers.

---
+++ Assets/0_DontFallRoot/Scripts/Game/HandController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class HandController : MonoBehaviour {
+
+	[SerializeField] private OVRInput.Controller _Controller;
+	[SerializeField] private Transform _MetacarpalTransform;
+	[SerializeField] private Transform _ProximalTransform;
+	[SerializeField] private Transform _IntermediateTransform;
+
+
+	#region unity loop
+	// Update is called once per frame
+	void Update () {
+		VrUpdateTransform();
+	}
+	#endregion
+
+
+	void VrUpdateTransform() {
+		_MetacarpalTransform.position =  OVRInput.GetLocalControllerPosition(_Controller);
+		_MetacarpalTransform.rotation = OVRInput.GetLocalControllerRotation(_Controller);
+	}
+}
---
+++ Assets/0_DontFallRoot/Scripts/Game/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class PlayerController : MonoBehaviour {
+
+#region Unity LifeCycle
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		OVRInput.GetLocalControllerPosition(OVRInput.Controller.LTouch); 
+	}
+#endregion
+
+}
